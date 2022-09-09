7c8508453212f67ecc6a29bcded03ec5cd167941
Daniel Huaman <danielsistemas20@gmail.com>
2022-09-09 02:43:34 -0500

    clase de servicio para subir una imagen al servidor

---
+++ src/main/java/com/curso/ecommerce/service/UploadFileService.java
@@ -0,0 +1,42 @@
+package com.curso.ecommerce.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+// para tener dos metodos : 1. subir 2, eliminar 
+
+@Service
+public class UploadFileService {
+	
+	private String folder="images//";
+	
+	public String saveImage( MultipartFile file ) throws IOException {
+		
+		if(!file.isEmpty()) {
+			byte [] bytes=file.getBytes();
+			Path path = Paths.get(folder+file.getOriginalFilename());
+			// guarda la imagen
+			Files.write(path,bytes);
+			return file.getOriginalFilename();
+		}
+		// en caso no lo suba
+		return "default.jpg";
+	}
+	
+	
+	// eliminar una imagen cuando eliminemos un producto
+	public void deleteImage(String nombre) {
+		
+		String ruta="images//";
+		File file=new File(ruta+nombre);
+		file.delete();
+	}
+	
+}
